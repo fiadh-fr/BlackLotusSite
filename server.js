04226ab5597c3b44588c11fbcc2868e6edbf66d2
Nukenin <nukenin.endo@gmail.com>
2023-04-24 01:30:16 +0400

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,8 @@ const http = require('http');
 const fs = require('fs')
 
 const token = `K5VJ3UhWlzhIfHElDxCP`;
-const options = {
-  key: fs.readFileSync('./ssl/key.pem', 'utf8'),
-  cert: fs.readFileSync('./ssl/cert.pem', 'utf8'),
-};
-const port = process.env.PORT || 3000;
 
 const app = express();
-
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(options, app);
-
 
 const config = {
   headers: { Authorization: `Bearer ${token}` }
@@ -43,15 +34,9 @@ app.get('/constellations', async (req, res) => {
   }
 });
 
-// Gérer les erreurs 404
-app.get('*', function(req, res){
-  res.send('404.html', 404);
+const port = process.env.PORT || 3000; // Utiliser le port 3000
+
+// Démarrer le serveur
+app.listen(port, () => {
+  console.log(`Le serveur est en cours d'exécution sur le port ${port}...`);
 });
-
-httpServer.listen(3000, () => {
-  console.log('Express server running on port 3000');
-})
-
-httpsServer.listen(443, () => {
-    console.log('HTTPS server running on port 443');
-})
