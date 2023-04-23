3d388b7a1b76d0ee91face8e68d22b093cc00bcc
Nukenin <nukenin.endo@gmail.com>
2023-04-23 23:22:27 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,26 +3,35 @@ const axios = require('axios');
 const path = require('path');
 const https = require('https');
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs')
 
-const token = 'K5VJ3UhWlzhIfHElDxCP';
-const port = process.env.PORT || 443;
+const token = `K5VJ3UhWlzhIfHElDxCP`;
+const options = {
+  key: fs.readFileSync('./ssl/key.pem', 'utf8'),
+  cert: fs.readFileSync('./ssl/cert.pem', 'utf8'),
+};
+const port = process.env.PORT || 443; // Mudei isso aqui
 
 const app = express();
 
-// Utilisation de serve-static pour servir les fichiers statiques
-const serveStatic = require('serve-static');
-app.use(serveStatic(path.join(__dirname, './')));
-
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer({
-  key: fs.readFileSync('./ssl/key.pem', 'utf8'),
-  cert: fs.readFileSync('./ssl/cert.pem', 'utf8')
-}, app);
+const httpsServer = https.createServer(options, app);
+
 
 const config = {
   headers: { Authorization: `Bearer ${token}` }
 };
+
+// Servir les fichiers statiques à partir de la racine du projet
+app.use(express.static(path.join(__dirname)));
+
+// Gérer la page d'accueil (index.html par défaut)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Servir les fichiers statiques à partir du dossier /var/www/blacklotus
+app.use(express.static('/var/www/blacklotus'));
 
 app.get('/constellations', async (req, res) => {
   try {
@@ -34,17 +43,22 @@ app.get('/constellations', async (req, res) => {
   }
 });
 
-// Gérer les erreurs 404 avec une page personnalisée
-app.use(function(req, res, next) {
-  res.status(404).sendFile(path.join(__dirname, './', '404.html'));
+// Gérer les erreurs 404
+app.get('*', function(req, res){
+  res.send('404.html', 404);
 });
 
+/*
+// démarre le serveur
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Le serveur est en cours d'exécution sur le port ${port}...`);
+});
+
+ */
 httpServer.listen(80, () => {
   console.log('HTTP Server running on port 80');
-});
-
+})
 httpsServer.listen(port, () => {
-  console.log(`HTTPS Server running on port ${port}`);
-});
-
-console.log('Server started');
+    console.log('HTTPS Server running on port 443');
+})
+console.log(`Plary esteve aqui..`)
