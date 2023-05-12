f0a9edfb4aa826c91c6f1996e4b23666d1d840eb
Nukenin <nukenin.endo@gmail.com>
2023-05-12 09:32:57 +0400

    404 Error Page Update

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname)));
 
 // Gérer la page d'accueil (index.html par défaut)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.get('/constellations', async (req, res) => {
@@ -50,8 +50,8 @@ app.get('/constellations', async (req, res) => {
 });
 
 // Gérer les erreurs 404
-app.get('*', function(req, res){
-  res.send('404.html', 404);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
 
 /*
