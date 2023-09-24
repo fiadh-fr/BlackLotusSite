d862c0aab7de99b6ae68dd8866fddd098e7c3272
Nukenin Endo <nukenin.endo@gmail.com>
2023-09-24 21:11:40 +0400

    Black Lotus name changes

--- js/widget-bot.js
+++ js/widget-bot.js
@@ -1,13 +1,16 @@
-const widgetbot = document.createElement('script');
-widgetbot.setAttribute('src', 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3');
-widgetbot.setAttribute('async', '');
-widgetbot.setAttribute('defer', '');
+const widgetbot = document.createElement("script");
+widgetbot.setAttribute(
+  "src",
+  "https://cdn.jsdelivr.net/npm/@widgetbot/crate@3"
+);
+widgetbot.setAttribute("async", "");
+widgetbot.setAttribute("defer", "");
 
 document.body.appendChild(widgetbot);
 
 widgetbot.onload = () => {
   new Crate({
-    server: '896047806454837278', // Black Lotus
-    channel: '1123374694603698287' // #᲼᲼site-chat
+    server: "896047806454837278", // The Black Lotus
+    channel: "1123374694603698287", // #᲼᲼site-chat
   });
 };
